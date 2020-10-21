f8d0efbed9d34c64fad58e2f7bfed804668f2b16
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2020-10-21 12:01:05 +0200

    Remove part of cache for testing

--- src/main/java/eu/ec/doris/kohesio/controller/FacetDevController.java
+++ src/main/java/eu/ec/doris/kohesio/controller/FacetDevController.java
@@ -1919,74 +1919,73 @@ public class FacetDevController {
     System.out.println("Start recoursive");
     recursiveMap(null);
     System.out.println("end recoursive");
-    String[] countries = {
-            null,
-            "https://linkedopendata.eu/entity/Q15",
-            "https://linkedopendata.eu/entity/Q2",
-            "https://linkedopendata.eu/entity/Q25",
-            "https://linkedopendata.eu/entity/Q20",
-            "https://linkedopendata.eu/entity/Q13",
-            "https://linkedopendata.eu/entity/Q12",
-    };
-
-    for (String country : countries) {
-      int[] offset = {0,15,30,45,60,75,90,105,120,135,150};
-      for (int o : offset) {
-        Boolean[] orderStartDate = {null, true, false};
-        for (Boolean b : orderStartDate){
-          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, null, null, null, 15, o, null);
-        }
-        Boolean[] orderEndDate = {null, true, false};
-        for (Boolean b : orderEndDate){
-          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, null, null, 15, o, null);
-        }
-        Boolean[] orderEuBudget = {null, true, false};
-        for (Boolean b : orderEuBudget){
-          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, null, 15, o, null);
-        }
-        Boolean[] orderTotalBudget = {null, true, false};
-        for (Boolean b : orderTotalBudget) {
-          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, 15, o, null);
-        }
-      }
-    }
-
-    for (String country : countries) {
-      euSearchBeneficiaries("en", null, country, null, null, null, null,null,null);
-    }
-    for (String country : countries) {
-      if (country!=null) {
-        JSONArray regions = facetEuRegions(country, "en");
-        regions.add(null);
-        for (Object region : regions) {
-          JSONArray funds = facetEuFunds("en");
-          funds.add(null);
-          for (Object fund : funds) {
-            JSONArray programs = facetEuPrograms("en", country);
-            programs.add(null);
-            for (Object program : programs) {
-              String r = null;
-              if (region != null) {
-                r = ((JSONObject) region).get("region").toString();
-              }
-              String f = null;
-              if (fund != null) {
-                f = ((JSONObject) fund).get("instance").toString();
-              }
-              String p = null;
-              if (program != null) {
-                p = ((JSONObject) program).get("instance").toString();
-              }
-              System.out.println("euSearchBeneficiaries");
-              euSearchBeneficiaries(
-                      "en", null, country, r, null, null, f, p, null);
-              euSearchProjectMap("en", null, country, null, f, p, null,null,null,null,null,null,null,null,null,null,null,null,r,r,null,0,400,null);
-              System.out.println("Done");
-            }
-          }
-        }
-      }
-    }
+//    String[] countries = {
+//            null,
+//            "https://linkedopendata.eu/entity/Q15",
+//            "https://linkedopendata.eu/entity/Q2",
+//            "https://linkedopendata.eu/entity/Q25",
+//            "https://linkedopendata.eu/entity/Q20",
+//            "https://linkedopendata.eu/entity/Q13",
+//            "https://linkedopendata.eu/entity/Q12",
+//    };
+//
+//    for (String country : countries) {
+//      int[] offset = {0,15,30,45,60,75,90,105,120,135,150};
+//      for (int o : offset) {
+//        Boolean[] orderStartDate = {null, true, false};
+//        for (Boolean b : orderStartDate){
+//          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, null, null, null, 15, o, null);
+//        }
+//        Boolean[] orderEndDate = {null, true, false};
+//        for (Boolean b : orderEndDate){
+//          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, null, null, 15, o, null);
+//        }
+//        Boolean[] orderEuBudget = {null, true, false};
+//        for (Boolean b : orderEuBudget){
+//          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, null, 15, o, null);
+//        }
+//        Boolean[] orderTotalBudget = {null, true, false};
+//        for (Boolean b : orderTotalBudget) {
+//          euSearchProject("en", null, country, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, b, null, null, null, 15, o, null);
+//        }
+//      }
+//    }
+//
+//    for (String country : countries) {
+//      euSearchBeneficiaries("en", null, country, null, null, null, null,null,null);
+//    }
+//    for (String country : countries) {
+//      if (country!=null) {
+//        JSONArray regions = facetEuRegions(country, "en");
+//        regions.add(null);
+//        for (Object region : regions) {
+//          JSONArray funds = facetEuFunds("en");
+//          funds.add(null);
+//          for (Object fund : funds) {
+//            JSONArray programs = facetEuPrograms("en", country);
+//            programs.add(null);
+//            for (Object program : programs) {
+//              String r = null;
+//              if (region != null) {
+//                r = ((JSONObject) region).get("region").toString();
+//              }
+//              String f = null;
+//              if (fund != null) {
+//                f = ((JSONObject) fund).get("instance").toString();
+//              }
+//              String p = null;
+//              if (program != null) {
+//                p = ((JSONObject) program).get("instance").toString();
+//              }
+//              euSearchBeneficiaries(
+//                      "en", null, country, r, null, null, f, p, null);
+//              euSearchProjectMap("en", null, country, null, f, p, null,null,null,null,null,null,null,null,null,null,null,null,r,r,null,0,400,null);
+//              System.out.println("Done");
+//            }
+//          }
+//        }
+//      }
+//    }
   }
 
   void recursiveMap(String granularityRegion) throws Exception {
