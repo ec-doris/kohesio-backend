c00ec717c532c9e8261e5d158df6bdb8ebdedda3
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2021-10-28 20:29:30 +0200

    Adding labels for spain and germany of the beneficiaries

--- src/main/java/eu/ec/doris/kohesio/controller/BeneficiaryController.java
+++ src/main/java/eu/ec/doris/kohesio/controller/BeneficiaryController.java
@@ -98,6 +98,8 @@ public class BeneficiaryController {
                 "              || (LANG(?beneficiaryLabel) = \"cs\" && ?country = <https://linkedopendata.eu/entity/Q25> ) \n" +
                 "              || (LANG(?beneficiaryLabel) = \"el\" && ?country = <https://linkedopendata.eu/entity/Q17> ) \n" +
                 "              || (LANG(?beneficiaryLabel) = \"el\" && ?country = <https://linkedopendata.eu/entity/Q31> ) \n" +
+                "              || (LANG(?beneficiaryLabel) = \"de\" && ?country = <https://linkedopendata.eu/entity/Q22> ) \n" +
+                "              || (LANG(?beneficiaryLabel) = \"es\" && ?country = <https://linkedopendata.eu/entity/Q7> ) \n" +
                 "              || (LANG(?beneficiaryLabel) = \"sv\" && ?country = <https://linkedopendata.eu/entity/Q11> ) \n" +
                 "              || (LANG(?beneficiaryLabel) = \"hr\" && ?country = <https://linkedopendata.eu/entity/Q30> ) \n" +
                 "              || (LANG(?beneficiaryLabel) = \"ro\" && ?country = <https://linkedopendata.eu/entity/Q28> ) \n" +
