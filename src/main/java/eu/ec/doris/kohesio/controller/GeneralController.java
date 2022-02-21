dde95ff6d9a2b6da154f6ccd704128a3c074689f
J.M <jonathan.mallet3872@gmail.com>
2022-02-21 12:06:47 +0100

    add CORS

--- src/main/java/eu/ec/doris/kohesio/controller/GeneralController.java
+++ src/main/java/eu/ec/doris/kohesio/controller/GeneralController.java
@@ -13,11 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,6 +37,11 @@ public class GeneralController {
 
     @Value("${kohesio.sparqlEndpoint}")
     String sparqlEndpoint;
+
+    @ModelAttribute
+    public void setVaryResponseHeader(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+    }
 
     @GetMapping(value = "/facet/eu/search/general", produces = "application/json")
     public ResponseEntity euSearchGeneral(
