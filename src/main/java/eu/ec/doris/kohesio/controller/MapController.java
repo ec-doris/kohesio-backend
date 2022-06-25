f44e0880ff88efbc31d16abcd58b90241e6e4c5e
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2022-06-25 20:46:51 +0200

    New logic map controller

--- src/main/java/eu/ec/doris/kohesio/controller/MapController.java
+++ src/main/java/eu/ec/doris/kohesio/controller/MapController.java
@@ -123,8 +123,8 @@ public class MapController {
 
         if ((latitude != null && longitude != null)
                 || ((granularityRegion != null && "country".equals(facetController.nutsRegion.get(granularityRegion).granularity) && "https://linkedopendata.eu/entity/Q2".equals(facetController.nutsRegion.get(granularityRegion).country))
-                && !(granularityRegion != null && "country".equals(facetController.nutsRegion.get(granularityRegion).granularity) && "https://linkedopendata.eu/entity/Q11".equals(facetController.nutsRegion.get(granularityRegion).country))
-                && (numResults <= 2000 || (granularityRegion != null && facetController.nutsRegion.get(granularityRegion).narrower.size() == 0)))
+                || !(granularityRegion != null && "country".equals(facetController.nutsRegion.get(granularityRegion).granularity) && "https://linkedopendata.eu/entity/Q11".equals(facetController.nutsRegion.get(granularityRegion).country))
+                || (numResults <= 2000 || (granularityRegion != null && facetController.nutsRegion.get(granularityRegion).narrower.size() == 0)))
         ) {
             return mapReturnCoordinates(search, country, region, granularityRegion, latitude, longitude, limit, offset, timeout);
         } else {
