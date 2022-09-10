54138f914563d30dda18b9867e380a9fdf577edf
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2022-09-10 19:25:34 +0200

    Simplify query

--- src/main/java/eu/ec/doris/kohesio/controller/ProjectController.java
+++ src/main/java/eu/ec/doris/kohesio/controller/ProjectController.java
@@ -1013,9 +1013,9 @@ public class ProjectController {
                         + " OPTIONAL { ?s0 <https://linkedopendata.eu/prop/direct/P32> ?country . OPTIONAL {?country <https://linkedopendata.eu/prop/direct/P173> ?countrycode . }} "
                         + " OPTIONAL {?s0 <https://linkedopendata.eu/prop/direct/P1848> ?objective. OPTIONAL {?objective <https://linkedopendata.eu/prop/direct/P1105> ?objectiveId.} } "
                         + " OPTIONAL {?s0 <https://linkedopendata.eu/prop/direct/P888> ?category .  OPTIONAL {?category <https://linkedopendata.eu/prop/direct/P1848> ?objective. OPTIONAL {?objective <https://linkedopendata.eu/prop/direct/P1105> ?objectiveId.} } } "
-                        + " OPTIONAL { ?s0 <https://linkedopendata.eu/prop/direct/P836> ?summary. "
-                        + " FILTER(LANG(?summary)=\"" + language + "\")"
-                        + "} "
+//                        + " OPTIONAL { ?s0 <https://linkedopendata.eu/prop/direct/P836> ?summary. "
+//                        + " FILTER(LANG(?summary)=\"" + language + "\")"
+//                        + "} "
                         + "} ";
 
 
