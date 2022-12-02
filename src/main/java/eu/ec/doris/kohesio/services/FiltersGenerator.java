d32e8f1961e885af3537c73515c6d1c0ab7469c9
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2022-12-02 09:28:38 +0100

    Adding more default languages for beneficiaries

--- src/main/java/eu/ec/doris/kohesio/services/FiltersGenerator.java
+++ src/main/java/eu/ec/doris/kohesio/services/FiltersGenerator.java
@@ -219,7 +219,7 @@ public class FiltersGenerator {
         mapping.put("<https://linkedopendata.eu/entity/Q11>", Arrays.asList("sv")); // sweden
         mapping.put("<https://linkedopendata.eu/entity/Q7>", Arrays.asList("es")); // spain
         mapping.put("<https://linkedopendata.eu/entity/Q22>", Arrays.asList("de")); // germany
-        mapping.put("<https://linkedopendata.eu/entity/Q31>", Arrays.asList("el")); // cyprus
+        mapping.put("<https://linkedopendata.eu/entity/Q31>", Arrays.asList("el","en")); // cyprus
         mapping.put("<https://linkedopendata.eu/entity/Q17>", Arrays.asList("el")); // greece
         mapping.put("<https://linkedopendata.eu/entity/Q25>", Arrays.asList("cs")); // czech republic
         mapping.put("<https://linkedopendata.eu/entity/Q13>", Arrays.asList("pl")); // poland
@@ -227,7 +227,7 @@ public class FiltersGenerator {
         mapping.put("<https://linkedopendata.eu/entity/Q20>", Arrays.asList("fr")); // france
         mapping.put("<https://linkedopendata.eu/entity/Q2>", Arrays.asList("en")); // ireland
         mapping.put("<https://linkedopendata.eu/entity/Q32>", Arrays.asList("en")); // malta
-        mapping.put("<https://linkedopendata.eu/entity/Q8>", Arrays.asList("fr", "nl", "de")); // belgium
+        mapping.put("<https://linkedopendata.eu/entity/Q8>", Arrays.asList("fr", "nl", "de", "en")); // belgium
 
         return mapping;
     }
