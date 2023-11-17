f5eaae7c3cabe5562da937dd73958e22943fd7e2
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2023-11-17 14:41:32 +0100

    Issue #242

--- src/main/java/eu/ec/doris/kohesio/controller/FacetController.java
+++ src/main/java/eu/ec/doris/kohesio/controller/FacetController.java
@@ -1331,7 +1331,8 @@ public class FacetController {
 
     public List<String> projectTypes = Arrays.asList(
             "https://linkedopendata.eu/entity/Q3402194",
-            "https://linkedopendata.eu/entity/Q6714233"
+            "https://linkedopendata.eu/entity/Q6714233",
+            "https://linkedopendata.eu/entity/Q6869909"
     );
 
     @GetMapping(value = "/facet/eu/project_types", produces = "application/json")
